9a522405a1915bbfa53eac76a3cfa195e72697c0
Syldris <131539895+Syldris@users.noreply.github.com>
2025-04-17 04:47:42 +0900

    [Bronze II] Title: 정수의 개수, Time: 52 ms, Memory: 5612 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10821. 정수의 개수/정수의 개수.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class NewEmptyCSharpScript
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split(',');
+
+        Console.WriteLine(inputs.Count());
+    }
+}
