23ba577bd369b60c552c88bf84041b77779fc3e5
Syldris <131539895+Syldris@users.noreply.github.com>
2025-04-19 00:08:36 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 900 ms, Memory: 46972 KB -BaekjoonHub

---
+++ C#/백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+public class NewEmptyCSharpScript
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split();
+        int n = int.Parse(inputs[0]);
+        int m = int.Parse(inputs[1]);
+        int k = int.Parse(inputs[2]);
+        int[,] arr = new int[m, n];
+        bool[,,] visited = new bool[m, n, k + 1];
+
+        for (int y = 0; y < n; y++)
+        {
+            string line = Console.ReadLine();
+            for (int x = 0; x < m; x++)
+            {
+                arr[x, y] = line[x] - '0';
+            }
+        }
+
+        Queue<(int posX, int posY, int time, int breakNum, bool isMorning)> queue = new Queue<(int, int, int, int, bool)>();
+
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+
+        queue.Enqueue((0, 0, 1, 0, true));
+        visited[0, 0, 0] = true;
+
+        while (queue.Count > 0)
+        {
+            (int x, int y, int time, int breakNum, bool isMorning) = queue.Dequeue();
+
+            if (x == m - 1 && y == n - 1)
+            {
+                Console.WriteLine(time);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int px = x + dx[i];
+                int py = y + dy[i];
+                if (px >= 0 && py >= 0 && px < m && py < n)
+                {
+                    if (arr[px, py] == 1 && breakNum < k && !visited[px, py, breakNum + 1])
+                    {
+                        if(isMorning)
+                        {
+                            visited[px, py, breakNum + 1] = true;
+                            queue.Enqueue((px, py, time + 1, breakNum + 1, !isMorning));
+                        }
+                        else
+                        {
+                            queue.Enqueue((x, y, time + 1, breakNum, !isMorning));
+                        }
+                    }
+                    else if (arr[px, py] == 0 && !visited[px, py, breakNum])
+                    {
+                        visited[px, py, breakNum] = true;
+                        queue.Enqueue((px, py, time + 1, breakNum, !isMorning));
+                    }
+                }
+            }
+        }
+    Console.WriteLine(-1);
+    }
+}
