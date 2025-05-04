9d5f8e4c3db86675a625a9e39dce94ba648c60d3
Syldris <131539895+Syldris@users.noreply.github.com>
2025-05-05 07:37:34 +0900

    [Silver II] Title: 최소 힙, Time: 92 ms, Memory: 14116 KB -BaekjoonHub

---
+++ C#/백준/Silver/1927. 최소 힙/최소 힙.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        PriorityQueue<long, long> queue = new PriorityQueue<long, long>();
+        StringBuilder sb = new StringBuilder();
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if(number == 0)
+            {
+                if(queue.TryDequeue(out long a, out long b))
+                {
+                    sb.AppendLine(a.ToString());
+                }
+                else
+                {
+                    sb.AppendLine("0");
+                }
+            }
+            else
+            {
+                queue.Enqueue(number, number);
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
