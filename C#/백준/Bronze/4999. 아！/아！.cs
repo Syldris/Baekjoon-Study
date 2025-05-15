8c67a3f3ebbe4d93071075ea5f4a4be8f839eeeb
Syldris <131539895+Syldris@users.noreply.github.com>
2025-05-15 19:55:49 +0900

    [Bronze V] Title: 아!, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ C#/백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        Console.WriteLine(a.Length >= b.Length ? "go" : "no"); 
+    }
+}
