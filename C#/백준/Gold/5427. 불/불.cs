4c28bf20181957b989326d28ef210b9e5ff6a226
Syldris <131539895+Syldris@users.noreply.github.com>
2025-05-24 04:44:39 +0900

    [Gold IV] Title: 불, Time: 364 ms, Memory: 36008 KB -BaekjoonHub

---
+++ C#/백준/Gold/5427. 불/불.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Numerics;
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int tastcase = int.Parse(sr.ReadLine());
+        for (int t = 0; t < tastcase; t++)
+        {
+            string[] input = sr.ReadLine().Split(' ');  
+            int m = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            char[,] board = new char[m, n];
+
+            (int x, int y) startPos = (0, 0);
+            List<(int x, int y)> firelist = new List<(int, int)>();
+            int[,] fireTime = new int[m, n];
+            for(int y = 0; y < n; y++)
+            {
+                for(int x = 0; x< m; x++)
+                {
+                    fireTime[x,y] = int.MaxValue;
+                }
+            }
+
+
+            for (int y = 0; y < n; y++)
+            {
+                string line = sr.ReadLine();
+                for (int x = 0; x < m; x++)
+                {
+                    board[x, y] = line[x];
+                    if (line[x] == '*')
+                        firelist.Add((x,y));
+                    if (line[x] == '@')
+                    {
+                        startPos = (x, y);
+                        board[x, y] = '.';
+                    }
+                }
+            }
+
+            int[] dx = { -1, 1, 0, 0 };
+            int[] dy = { 0, 0, -1, 1 };
+
+            Queue<(int x, int y, int sec)> firequeue = new Queue<(int, int, int)>();
+            foreach (var fire in firelist)
+            {
+                fireTime[fire.x, fire.y] = 0;
+                firequeue.Enqueue((fire.x, fire.y, 0));
+            }
+            while (firequeue.Count > 0)
+            {
+                (int x, int y, int sec) = firequeue.Dequeue();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int px = x + dx[i];
+                    int py = y + dy[i];
+                    if (px < 0 || py < 0 || px >= m || py >= n)
+                    {
+                        continue;
+                    }
+
+                    if (board[px, py] == '.' && sec + 1< fireTime[px,py])
+                    {
+                        fireTime[px,py] = sec + 1;
+                        firequeue.Enqueue((px, py, sec + 1));
+                    }
+                }
+            }
+
+            int[,] visited = new int [m,n];
+            for(int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < m; x++)
+                {
+                    visited[x,y] = int.MaxValue;
+                }
+            }
+            visited[startPos.x, startPos.y] = 0;
+
+            PriorityQueue<(int x, int y, int sec),int> queue = new PriorityQueue<(int, int, int),int>();
+            queue.Enqueue((startPos.x, startPos.y, 0),0);
+            bool escape = false;
+
+            while (queue.Count > 0)
+            {
+                (int x, int y, int sec) = queue.Dequeue();
+
+                if(escape)
+                    break;
+
+                for(int i = 0; i < 4;  i++)
+                {
+                    int px = x + dx[i];
+                    int py = y + dy[i];
+                    if(px < 0 || py < 0 || px >= m || py >= n)
+                    {
+                        sw.WriteLine(++sec);
+                        escape = true;
+                        break;
+                    }
+
+                    if (board[px, py] == '.' && sec+1 < fireTime[px,py] && sec + 1 < visited[px,py])
+                    {
+                        visited[px, py] = sec + 1;
+                        queue.Enqueue((px, py, sec + 1), sec + 1);
+                    }
+                }
+            }
+            if(!escape)
+                sw.WriteLine("IMPOSSIBLE");
+        }
+    }
+}
