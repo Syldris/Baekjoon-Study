f1e895bdf677bd49a826ceda14cd964b3352e30d
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-04 00:28:45 +0900

    [Platinum V] Title: 비숍, Time: 116 ms, Memory: 6188 KB -BaekjoonHub

---
+++ C#/백준/Platinum/1799. 비숍/비숍.cs
@@ -0,0 +1,80 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        bool[] diag1 = new bool[2 * n - 1];
+        bool[] diag2 = new bool[2 * n - 1];
+        bool[,] board = new bool[n, n];
+
+        List<(int x, int y)> blacks = new List<(int x, int y)>();
+        List<(int x, int y)> whites = new List<(int x, int y)>();
+
+        int value = 0;
+        int maxValue = 0;
+        for (int y = 0; y < n; y++)
+        {
+            int[] line = sr.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int x = 0; x < n; x++)
+            {
+                if (line[x] == 1)
+                    board[x, y] = true;
+                if ((x + y) % 2 == 0)
+                    blacks.Add((x, y));
+                else
+                    whites.Add((x, y));
+            }
+        }
+        int blackValue = 0;
+        int whiteValue = 0;
+
+        DFS(0, blacks);
+
+        blackValue = maxValue;
+        value = 0;
+        maxValue = 0;
+
+        Array.Clear(diag1, 0, diag1.Length);
+        Array.Clear(diag2, 0, diag2.Length);
+
+        DFS(0, whites);
+        whiteValue = maxValue;
+        sw.Write(blackValue + whiteValue);
+
+        void DFS(int index, List<(int x,int y)> list)
+        {
+            if (index == list.Count)
+            {
+                maxValue = Math.Max(maxValue, value);
+                return;
+            }
+            (int x, int y) = list[index];
+            if (CanPlace(x, y))
+            {
+                diag1[x - y + n - 1] = true;
+                diag2[x + y] = true;
+                value++;
+                DFS(index + 1, list);
+                diag1[x - y + n - 1] = false;
+                diag2[x + y] = false;
+                value--;
+            }
+            DFS(index + 1, list);
+        }
+
+        bool CanPlace(int x, int y)
+        {
+            if (!board[x, y])
+                return false;
+            else if (diag1[x - y + n - 1])
+                return false;
+            else if (diag2[x + y])
+                return false;
+            return true;
+        }
+    }
+}
