476546768fb4c1d9df3d31495d25caf0c33a44c5
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-05 00:36:29 +0900

    [Bronze IV] Title: Periods, Time: 48 ms, Memory: 5600 KB -BaekjoonHub

---
+++ C#/백준/Bronze/26560. Periods/Periods.cs
@@ -0,0 +1,16 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = sr.ReadLine();
+            sw.WriteLine(input[^1] == '.' ? input : input + '.');
+        }
+    }
+}
