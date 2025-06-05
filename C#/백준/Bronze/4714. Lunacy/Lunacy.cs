a5cba3da4db65e3803bf4721904b68bd407956aa
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-06 06:01:36 +0900

    [Bronze IV] Title: Lunacy, Time: 52 ms, Memory: 6352 KB -BaekjoonHub

---
+++ C#/백준/Bronze/4714. Lunacy/Lunacy.cs
@@ -0,0 +1,18 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        while (true)
+        {
+            float weight = float.Parse(sr.ReadLine());
+            if(weight < 0)
+                break;
+            float moonWeight = weight * 0.167f;
+            sw.WriteLine($"Objects weighing {weight.ToString("F2")} on Earth will weigh {moonWeight.ToString("F2")} on the moon.");
+        }
+    }
+}
