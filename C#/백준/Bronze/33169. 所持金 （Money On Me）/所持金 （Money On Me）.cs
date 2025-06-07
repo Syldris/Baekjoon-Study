ecb321c3e42b3118f5c9214038b643cc14ec445d
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-08 00:31:26 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cs
@@ -0,0 +1,13 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int a = int.Parse(sr.ReadLine());
+        int b = int.Parse(sr.ReadLine());
+        sw.Write(a*1000 + b*10000);
+    }
+}
