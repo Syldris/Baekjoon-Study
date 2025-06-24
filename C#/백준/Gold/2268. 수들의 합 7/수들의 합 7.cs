4703ff8053d38f5bff5467771fe1e4df86cbb431
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-25 04:49:04 +0900

    [Gold I] Title: 수들의 합 7, Time: 1120 ms, Memory: 44888 KB -BaekjoonHub

---
+++ C#/백준/Gold/2268. 수들의 합 7/수들의 합 7.cs
@@ -0,0 +1,70 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        long[] tree = new long[n * 4];
+
+        for (int i = 0; i < m; i++)
+        {
+            string[] line = sr.ReadLine().Split();
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            int c = int.Parse(line[2]);
+            if (a == 0)
+            {
+                if (b > c) (b, c) = (c, b);
+                sw.WriteLine(Query(1, 1, n, b, c));
+            }
+            else
+            {
+                Update(1, 1, n, b, c);
+            }
+        }
+
+        void Update(int node, int start, int end, int index, int value)
+        {
+            if(start > index || end < index)
+                return;
+
+            if (start == end)
+            {
+                tree[node] = value;
+                return;
+            }
+
+            int mid = (start + end) / 2;
+            if (index <= mid)
+            {
+                Update(node * 2, start, mid, index, value);
+            }
+            else
+            {
+                Update(node * 2 + 1, mid + 1, end, index, value);
+            }
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+
+        long Query(int node, int start, int end, int left, int right)
+        {
+            if(start > right || left > end)
+                return 0;
+
+            if (left <= start && end <= right)
+            {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+
+            return Query(node * 2, start, mid, left, right) + Query(node * 2 + 1, mid + 1, end, left, right);
+        }
+    }
+}
