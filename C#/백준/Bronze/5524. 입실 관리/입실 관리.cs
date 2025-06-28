95baa4f9ce977e1ce45c761daf0ba253d381684e
Syldris <131539895+Syldris@users.noreply.github.com>
2025-06-29 05:22:42 +0900

    [Bronze IV] Title: 입실 관리, Time: 56 ms, Memory: 5616 KB, Score: 100 point -BaekjoonHub

---
+++ C#/백준/Bronze/5524. 입실 관리/입실 관리.cs
@@ -0,0 +1,17 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = sr.ReadLine();
+            sw.WriteLine(input.ToLower());
+        }
+    }
+}
