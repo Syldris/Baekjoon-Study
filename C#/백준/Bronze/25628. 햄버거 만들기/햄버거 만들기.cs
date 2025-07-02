35a7ee0661c9fa9d7dcb9df478d15b59983bb2e3
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-02 21:19:13 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 52 ms, Memory: 5536 KB -BaekjoonHub

---
+++ C#/백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cs
@@ -0,0 +1,15 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        sw.Write(Math.Min(n / 2, m));
+    }
+}
