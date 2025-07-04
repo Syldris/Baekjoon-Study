c998ebc81f3308e07051bbc181b1bf9893f29d53
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-04 23:18:06 +0900

    [Bronze V] Title: We love kriii, Time: 44 ms, Memory: 5340 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,12 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.Write("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
