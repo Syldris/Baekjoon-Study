cc071441c05a874e8f410370fac9ed44e910e06c
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-06 16:52:05 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,13 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        sw.Write(n-543);
+    }
+}
