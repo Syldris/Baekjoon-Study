4fea93897fa94ff0aaecbdc2ef535d5ecfa33d78
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-13 20:57:10 +0900

    [Bronze IV] Title: 양말 짝 맞추기, Time: 48 ms, Memory: 5540 KB -BaekjoonHub

--- C#/백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.cs
+++ C#/백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.cs
@@ -9,37 +9,11 @@ class Program
         using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-
-        Dictionary<int, int> freq = new Dictionary<int, int>();
+        int result = 0;
         for (int i = 0; i < 5; i++)
         {
-            int num = int.Parse(sr.ReadLine());
-
-            if (freq.TryGetValue(num, out int value))
-            {
-                freq[num]++;
-            }
-            else
-            {
-                freq.Add(num, 1);
-            }
+            result ^= int.Parse(sr.ReadLine());
         }
-        List<int> list = freq.Where(x => x.Value == 1).Select(x => x.Key).ToList();
-
-        if (list.Count > 0)
-        {
-            sw.Write(list[0]);
-        }
-        else
-        {
-            list = freq.Where(x => x.Value == 3).Select(x => x.Key).ToList();
-            if (list.Count > 0)
-                sw.Write(list[0]);
-            else
-            {
-                list = freq.Where(x => x.Value == 5).Select(x => x.Key).ToList();
-                sw.Write(list[0]);
-            }
-        }
+        sw.Write(result);
     }
 }
