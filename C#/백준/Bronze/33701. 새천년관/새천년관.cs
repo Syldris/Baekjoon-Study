313a6644bff54e41a565bf62d2339a1a197d1a43
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-13 20:33:13 +0900

    [Bronze IV] Title: 새천년관, Time: 48 ms, Memory: 5344 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33701. 새천년관/새천년관.cs
@@ -0,0 +1,14 @@
+#nullable disable
+using System;
+using System.Collections;
+using System.Numerics;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        sw.Write("nkugwan");
+    }
+}
