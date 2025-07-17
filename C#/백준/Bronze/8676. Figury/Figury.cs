9ed8000b0405088958a1b433d41915f4a3dc94df
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-18 02:38:53 +0900

    [Bronze II] Title: Figury, Time: 196 ms, Memory: 65016 KB -BaekjoonHub

--- C#/백준/Bronze/8676. Figury/Figury.cs
+++ C#/백준/Bronze/8676. Figury/Figury.cs
@@ -10,18 +10,34 @@ class Program
         int n = int.Parse(sr.ReadLine());
         int[] arr = sr.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-        for (int i = 0; i <= n-3 ; i++)
+        int one = 0;
+        int two = 0;
+        int three = 0;
+
+        for (int i = 0; i < n; i++)
         {
-            bool[] bools = new bool[3];
-            bools[arr[i]] = true;
-            bools[arr[i + 1]] = true;
-            bools[arr[i+2]] = true;
-            if (bools[0] && bools[1] && bools[2])
+            if (i >= 3)
             {
-                sw.WriteLine("TAK");
+                if (arr[i - 3] == 1)
+                    one--;
+                else if (arr[i - 3] == 2)
+                    two--;
+                else
+                    three--;
+            }
+            if (arr[i] == 1)
+                one++;
+            else if (arr[i] == 2)
+                two++;
+            else
+                three++;
+
+            if (one > 0 && two > 0 && three > 0)
+            {
+                sw.Write("TAK");
                 return;
             }
         }
-        sw.WriteLine("NIE");
+        sw.Write("NIE");
     }
 }
