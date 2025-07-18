1fcdf098a1ab881af9272e951d12153eda45c1c1
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-19 01:05:13 +0900

    [Bronze V] Title: Metronome, Time: 48 ms, Memory: 5524 KB -BaekjoonHub

---
+++ C#/백준/Bronze/27389. Metronome/Metronome.cs
@@ -0,0 +1,13 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        double d = double.Parse(sr.ReadLine());
+        sw.Write(d/4);
+    }
+}
