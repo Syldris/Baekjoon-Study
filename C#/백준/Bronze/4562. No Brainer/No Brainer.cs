ff99ebd284afe7d9a2779936a170c8fdf4a88fa5
Syldris <131539895+Syldris@users.noreply.github.com>
2025-07-25 02:37:32 +0900

    [Bronze IV] Title: No Brainer, Time: 52 ms, Memory: 5848 KB -BaekjoonHub

---
+++ C#/백준/Bronze/4562. No Brainer/No Brainer.cs
@@ -0,0 +1,20 @@
+#nullable disable
+using System;
+using System.Numerics;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        int n = int.Parse(sr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = sr.ReadLine().Split();
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            sw.WriteLine(b > a ? "NO BRAINS" : "MMM BRAINS");
+        }
+    }
+}
