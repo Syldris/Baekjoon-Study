2c64338a46045a175109cb7439c3e16e04d8e671
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-04 14:15:47 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 72 ms, Memory: 5504 KB -BaekjoonHub

---
+++ C#/백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cs
@@ -0,0 +1,16 @@
+#nullable disable
+using System;
+using System.Net.Sockets;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        sw.WriteLine((long)n*n*n);
+        sw.WriteLine(3);
+    }
+}
