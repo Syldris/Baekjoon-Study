4f7b13739e764e346209658fd24a4dd7170c563f
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-13 07:34:01 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ C#/백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cs
@@ -0,0 +1,16 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        sw.Write(a * 2 >= b ? 'E' : 'H');
+    }
+}
