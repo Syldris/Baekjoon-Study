23f86cb39c6e740bf5a22f1760b183b562626332
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-14 23:01:24 +0900

    [Bronze II] Title: Winning ICPC, Time: 60 ms, Memory: 6864 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15845. Winning ICPC/Winning ICPC.cs
@@ -0,0 +1,41 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        int[] arr = sr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int[] result = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            int[] line = sr.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int j = 0; j < m; j++)
+            {
+                if (line[j] == arr[j])
+                {
+                    result[i]++;
+                }
+            }
+        }
+
+        int max = 0;
+        int team = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            if (result[i] > max)
+            {
+                team = i;
+                max = result[i];
+            }
+        }
+        sw.Write(team);
+    }
+}
