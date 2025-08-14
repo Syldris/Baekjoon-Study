cec76814c788babd9cc0d0cdf0ab262fffaaf14c
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-14 23:03:10 +0900

    [Bronze II] Title: Winning ICPC, Time: 56 ms, Memory: 6624 KB -BaekjoonHub

--- C#/백준/Bronze/15845. Winning ICPC/Winning ICPC.cs
+++ C#/백준/Bronze/15845. Winning ICPC/Winning ICPC.cs
@@ -10,13 +10,13 @@ class Program
         string[] input = sr.ReadLine().Split();
         int n = int.Parse(input[0]);
         int m = int.Parse(input[1]);
-        int[] arr = sr.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
 
         int[] result = new int[n + 1];
 
         for (int i = 1; i <= n; i++)
         {
-            int[] line = sr.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] line = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
             for (int j = 0; j < m; j++)
             {
                 if (line[j] == arr[j])
