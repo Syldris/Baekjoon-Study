3f048443a02936c001fba48ffdd1dff5a6684489
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-18 08:31:21 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.cs
@@ -0,0 +1,31 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] line = sr.ReadLine().Split();
+        int a = int.Parse(line[0]);
+        int b = int.Parse(line[1]); 
+        int c = int.Parse(line[2]);
+        if (a >= 1000)
+        {
+            if (b >= 8000 || c >= 260)
+            {
+                sw.Write("Very Good");
+            }
+            else
+            {
+                sw.Write("Good");
+            }
+        }
+        else
+        {
+            sw.Write("Bad");
+        }
+    }
+}
