10e70a81dcde843f02b90227bf52409e3d8846b1
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-21 22:42:50 +0900

    [Bronze IV] Title: 한글 2, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ C#/백준/Bronze/11283. 한글 2/한글 2.cs
@@ -0,0 +1,14 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        char c = sr.ReadLine()[0];
+        sw.Write(c - '가' + 1);
+    }
+}
