8d80cbda4c5d0e635d09671ee875e7308771a201
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-21 21:52:48 +0900

    [Bronze IV] Title: FYI, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ C#/백준/Bronze/17863. FYI/FYI.cs
@@ -0,0 +1,14 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        sw.Write(n / 10000 == 555 ? "YES" : "NO");
+    }
+}
