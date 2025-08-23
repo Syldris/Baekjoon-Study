7f19ad9f4999d41865ab4f0f86a05c79a2d7bd8b
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-23 17:19:44 +0900

    [Silver V] Title: 생일, Time: 56 ms, Memory: 6156 KB -BaekjoonHub

--- C#/백준/Silver/5635. 생일/생일.cs
+++ C#/백준/Silver/5635. 생일/생일.cs
@@ -3,21 +3,42 @@ using System;
 using System.Text;
 class Program
 {
+    public struct Data : IComparable<Data>
+    {
+        public string name;
+        public int day;
+        public int month;
+        public int year;
+        public int CompareTo(Data other)
+        {
+            if(year != other.year) return year.CompareTo(other.year);
+            else if(month != other.month) return month.CompareTo(other.month);
+            else  return day.CompareTo(other.day);
+        }
+    }
+
     static void Main()
     {
         using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
         int n = int.Parse(sr.ReadLine());
-        (string name, int day, int month, int year)[] data = new (string, int, int, int)[n];
+        Data[] data = new Data[n];
 
         for (int i = 0; i < n; i++)
         {
             string[] line = sr.ReadLine().Split();
-            data[i] = (line[0], int.Parse(line[1]), int.Parse(line[2]), int.Parse(line[3]));
+            data[i] = new Data 
+            { 
+                name = line[0],
+                day = int.Parse(line[1]),
+                month = int.Parse(line[2]),
+                year = int.Parse(line[3]),
+            };
         }
 
-        data = data.OrderBy(x => x.year).ThenBy(x => x.month).ThenBy(x => x.day).ToArray();
+        Array.Sort(data);
+
         sw.WriteLine(data[^1].name);
         sw.WriteLine(data[0].name);
     }
