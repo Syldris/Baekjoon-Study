87740ee11729f548db8db5b3e7d6ffa20af747ee
Syldris <131539895+Syldris@users.noreply.github.com>
2025-08-29 17:41:21 +0900

    [Bronze I] Title: 소수가 아닌 수 2, Time: 52 ms, Memory: 5784 KB, Score: 100 point -BaekjoonHub

--- C#/백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cs
+++ C#/백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cs
@@ -8,10 +8,13 @@ class Program
         using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
         using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
 
-        string input = sr.ReadLine();
-        double value = double.Parse(input);
+        string[] input = sr.ReadLine().Split('.');
+        int result = 10;
+        for (int i = 1; i < input[1].Length; i++)
+        {
+            result *= 10;
+        }
         sw.WriteLine("YES");
-        int result = (int)Math.Pow(10, input.Length - 2);
-        sw.WriteLine($"{value * result} {result}");
+        sw.WriteLine($"{int.Parse(input[1])} {result}");
     }
 }
