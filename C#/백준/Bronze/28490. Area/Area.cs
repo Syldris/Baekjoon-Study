2cb72db2f55a837632a6bd66e22f0c2b790304f6
Syldris <131539895+Syldris@users.noreply.github.com>
2025-09-05 19:26:00 +0900

    [Bronze IV] Title: Area, Time: 52 ms, Memory: 5884 KB, Score: 100 point -BaekjoonHub

---
+++ C#/백준/Bronze/28490. Area/Area.cs
@@ -0,0 +1,22 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        int result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = sr.ReadLine().Split();
+            int a = int.Parse(line[0]);
+            int b = int.Parse(line[1]);
+            result = Math.Max(a * b, result);
+        }
+        sw.Write(result);
+    }
+}
