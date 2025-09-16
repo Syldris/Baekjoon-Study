33575059fa74816314773e586312a4f859261567
Syldris <131539895+Syldris@users.noreply.github.com>
2025-09-16 22:17:27 +0900

    [Silver IV] Title: 베스트셀러, Time: 64 ms, Memory: 6116 KB -BaekjoonHub

---
+++ C#/백준/Silver/1302. 베스트셀러/베스트셀러.cs
@@ -0,0 +1,30 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        Dictionary<string, int> hash = new Dictionary<string, int>();
+        for (int i = 0; i < n; i++)
+        {
+            string line = sr.ReadLine();
+            if (hash.TryGetValue(line, out int hashValue))
+            {
+                hash[line] = hashValue + 1;
+            }
+            else
+            {
+                hash[line] = 1;
+            }
+        }
+        int max = hash.Values.Max();
+        string words = hash.Where(x => x.Value == max).Select(x => x.Key).Min();
+        sw.Write(words);
+
+    }
+}
