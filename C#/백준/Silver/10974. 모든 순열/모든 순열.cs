0526e25137dbdcd5f1efe1cabeb053b28efc6eb3
Syldris <131539895+Syldris@users.noreply.github.com>
2025-09-27 05:34:12 +0900

    [Silver III] Title: 모든 순열, Time: 68 ms, Memory: 9004 KB -BaekjoonHub

---
+++ C#/백준/Silver/10974. 모든 순열/모든 순열.cs
@@ -0,0 +1,37 @@
+#nullable disable
+using System;
+using System.Numerics;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        bool[] visited = new bool[n];
+        int[] arr = new int[n];
+
+        DFS(0);
+
+        void DFS(int depth)
+        {
+            if (depth == n)
+            {
+                sw.WriteLine(String.Join(' ', arr));
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (!visited[i-1])
+                {
+                    arr[depth] = i;
+                    visited[i-1] = true;
+                    DFS(depth + 1);
+                    visited[i-1] = false;
+                }
+            }
+        }
+    }
+}
