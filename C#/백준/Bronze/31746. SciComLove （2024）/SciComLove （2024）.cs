c4368e641823667d22e87e4150e9d396956c9878
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-01 13:10:11 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 48 ms, Memory: 5480 KB, Score: 100 point -BaekjoonHub

---
+++ C#/백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cs
@@ -0,0 +1,15 @@
+#nullable disable
+using System;
+using System.Numerics;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        sw.WriteLine(n % 2 == 0 ? "SciComLove" : "evoLmoCicS");
+    }
+}
