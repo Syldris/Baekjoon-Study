fc323fbdbb75094a202e195558772924e1ee5183
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-07 05:58:32 +0900

    [Bronze V] Title: 연세대학교, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,14 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        sw.Write(n == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
