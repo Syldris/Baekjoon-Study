ef083e10360be38342e6fc043f2f3e8da8080dd4
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-10 07:46:08 +0900

    [Gold IV] Title: 트리, Time: 120 ms, Memory: 20248 KB -BaekjoonHub

---
+++ C#/백준/Gold/4803. 트리/트리.cs
@@ -0,0 +1,89 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        int num = 0;
+        while (true)
+        {
+            string[] input = sr.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            if (n == 0 && m == 0)
+            {
+                return;
+            }
+
+            int[] parent = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                parent[i] = i;
+            }
+
+            HashSet<int> cycle = new HashSet<int>();
+            for (int i = 0; i < m; i++)
+            {
+                string[] line = sr.ReadLine().Split();
+                int a = int.Parse(line[0]);
+                int b = int.Parse(line[1]);
+                Union(a, b);
+            }
+
+            int tree = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int root = Find(i);
+                if (root == i && !cycle.Contains(root))
+                {
+                    tree++;
+                }
+            }
+
+            if (tree == 0)
+            {
+                sw.WriteLine($"Case {++num}: No trees.");
+            }
+            else if (tree == 1)
+            {
+                sw.WriteLine($"Case {++num}: There is one tree.");
+            }
+            else
+            {
+                sw.WriteLine($"Case {++num}: A forest of {tree} trees.");
+            }
+
+            int Find(int value)
+            {
+                if (parent[value] != value)
+                {
+                    parent[value] = Find(parent[value]);
+                }
+                return parent[value];
+            }
+
+            void Union(int a, int b)
+            {
+                int rootA = Find(a);
+                int rootB = Find(b);
+
+                if (rootA != rootB)
+                {
+                    if (cycle.Contains(rootA) || cycle.Contains(rootB))
+                    {
+                        cycle.Add(rootA);
+                        cycle.Add(rootB);
+                    }
+                    parent[rootA] = rootB;
+                }
+                else
+                {
+                    cycle.Add(rootA);
+                    cycle.Add(rootB);
+                }
+            }
+        }
+    }
+}
