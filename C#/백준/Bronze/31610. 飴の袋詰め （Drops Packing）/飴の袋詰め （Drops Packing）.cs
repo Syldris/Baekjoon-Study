068a7cf34aa7049d1557e6a0a6414bd701389bd4
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-14 06:10:50 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cs
@@ -0,0 +1,16 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int a = int.Parse(sr.ReadLine());
+        int b = int.Parse(sr.ReadLine());
+        int c = int.Parse(sr.ReadLine());
+        sw.Write(a * b + c);
+    }
+}
