ed7bb62e22bf2666bcc578f16a7f954e2cfe3ffa
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-22 04:05:19 +0900

    [Bronze II] Title: 경로당펑크 2077, Time: 52 ms, Memory: 5648 KB -BaekjoonHub

---
+++ C#/백준/Bronze/25205. 경로당펑크 2077/경로당펑크 2077.cs
@@ -0,0 +1,19 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+        string text = sr.ReadLine();
+
+        char[] arr = new char[] { 'q', 'a', 'z', 'w', 's', 'x', 'e', 'd', 'c', 'r', 'f', 'v', 't', 'g' };
+
+        sw.Write(arr.Contains(text[^1]) ? 1 : 0);
+
+    }
+}
