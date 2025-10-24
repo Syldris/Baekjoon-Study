74b8f867f3d504dc83b4eab1bfdad4896ffffd2d
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-24 09:33:55 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ C#/백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.cs
@@ -0,0 +1,18 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        int k = int.Parse(input[2]);
+
+        sw.Write(k * m + m);
+    }
+}
