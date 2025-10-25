10e61bab22abf11f021010d1054dc8ccff3bed81
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-26 05:55:33 +0900

    [Bronze III] Title: Not A + B, Time: 48 ms, Memory: 5712 KB -BaekjoonHub

---
+++ C#/백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cs
@@ -0,0 +1,19 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = sr.ReadLine();
+            sw.WriteLine(1);
+        }
+    }
+}
