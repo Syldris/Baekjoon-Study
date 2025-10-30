733db3eeb68d78f4702cc7623a2e2c818fd6d9ed
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-31 04:26:14 +0900

    [Silver V] Title: 건덕이의 돌탑, Time: 48 ms, Memory: 5604 KB -BaekjoonHub

---
+++ C#/백준/Silver/33703. 건덕이의 돌탑/건덕이의 돌탑.cs
@@ -0,0 +1,20 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+
+        long result = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            result += i;
+        }
+        sw.Write(result);
+    }
+}
