9924f003eae99a9e26377080f2273329c5bfd40e
Syldris <131539895+Syldris@users.noreply.github.com>
2025-10-31 17:26:43 +0900

    [Silver IV] Title: 당근 키우기, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Silver/20363. 당근 키우기/당근 키우기.cs
@@ -0,0 +1,20 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+
+        (a, b) = a >= b ? (a, b) : (b, a);
+
+        sw.WriteLine(a + b + b / 10);
+    }
+}
