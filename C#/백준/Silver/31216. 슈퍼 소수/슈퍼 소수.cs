f6f62a48175f79ad81f8022a2301f55bcc66b967
Syldris <131539895+Syldris@users.noreply.github.com>
2025-11-06 02:34:33 +0900

    [Silver V] Title: 슈퍼 소수, Time: 52 ms, Memory: 6244 KB -BaekjoonHub

---
+++ C#/백준/Silver/31216. 슈퍼 소수/슈퍼 소수.cs
@@ -0,0 +1,48 @@
+#nullable disable
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int testcase = int.Parse(sr.ReadLine());
+
+        bool[] prime = new bool[318138];
+        Array.Fill(prime, true);
+        prime[1] = false;
+
+        for (int i = 2; i < prime.Length; i++)
+        {
+            if (!prime[i])
+            {
+                continue;
+            }
+
+            for (int j = 2; i * j < prime.Length; j++)
+            {
+                prime[i * j] = false;
+            }
+        }
+
+        int[] supurprime = new int[3001];
+        int value = 1;
+        int index = 1;
+        for (int i = 2; i < prime.Length; i++)
+        {
+            if (prime[i] && prime[index++])
+            {
+                supurprime[value++] = i;
+            }
+        }
+
+        for (int i = 0; i < testcase; i++)
+        {
+            int n = int.Parse(sr.ReadLine());
+            sw.WriteLine(supurprime[n]);
+        }
+    }
+
+}
