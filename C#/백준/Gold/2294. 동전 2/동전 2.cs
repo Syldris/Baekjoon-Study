b78fc536428d38f010bcbd16fa9ab3458a144f65
Syldris <131539895+Syldris@users.noreply.github.com>
2025-11-10 05:28:38 +0900

    [Gold V] Title: 동전 2, Time: 56 ms, Memory: 6188 KB -BaekjoonHub

---
+++ C#/백준/Gold/2294. 동전 2/동전 2.cs
@@ -0,0 +1,41 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);
+
+        int[] arr = new int[n];
+        int[] coins = new int[k + 1];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(sr.ReadLine());
+        }
+
+        const int max = 1000000;
+        Array.Fill(coins, max);
+
+        foreach (var item in arr)
+        {
+            if(item <= k)
+                coins[item] = 1;
+        }
+
+        for (int i = 0; i <= k; i++)
+        {
+            foreach (var item in arr)
+            {
+                int index = i + item;
+                if(index <= k)
+                    coins[index] = Math.Min(coins[index], coins[i] + 1);
+            }
+        }
+        sw.Write(coins[k] == max ? -1 : coins[k]);
+    }
+}
