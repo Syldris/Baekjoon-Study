edf2b8966e3198b300162de080d01de0279b31b2
Syldris <131539895+Syldris@users.noreply.github.com>
2025-11-16 05:35:55 +0900

    [Bronze II] Title: 더하기, Time: 48 ms, Memory: 5752 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10822. 더하기/더하기.cs
@@ -0,0 +1,18 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        int[] arr = sr.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+        int result = 0;
+        foreach (var item in arr)
+        {
+            result += item;
+        }
+        sw.WriteLine(result);
+    }
+}
