f97c6f9738f313490a7b84c19bcc8feece5efedd
Syldris <131539895+Syldris@users.noreply.github.com>
2025-11-24 07:14:14 +0900

    [Gold III] Title: 소수의 연속합, Time: 116 ms, Memory: 13132 KB -BaekjoonHub

---
+++ C#/백준/Gold/1644. 소수의 연속합/소수의 연속합.cs
@@ -0,0 +1,80 @@
+#nullable disable
+using System;
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int n = int.Parse(sr.ReadLine());
+
+        bool[] isPrime = new bool[n + 1];
+        Array.Fill(isPrime, true);
+
+        int sqrt = (int)Math.Floor(Math.Sqrt(n));
+
+        for (int i = 2; i <= sqrt; i++)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                if (isPrime[j])
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<int> prime = new List<int>();
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                prime.Add(i);
+            }
+        }
+
+        if (prime.Count == 0)
+        {
+            sw.Write(0);
+            return;
+        }
+
+        int start = 0, end = 1;
+        int value = prime[start];
+        int result = 0;
+
+        while (start < prime.Count)
+        {
+            if (end < prime.Count)
+            {
+                if (value == n)
+                {
+                    result++;
+                    value += prime[end++];
+                }
+                else if (value > n)
+                {
+                    value -= prime[start++];
+                }
+                else
+                {
+                    value += prime[end++];
+                }
+            }
+            else
+            {
+                if (value == n)
+                {
+                    result++;
+                    break;
+                }
+                else
+                {
+                    value -= prime[start++];
+                }
+            }
+        }
+        sw.Write(result);
+    }
+}
