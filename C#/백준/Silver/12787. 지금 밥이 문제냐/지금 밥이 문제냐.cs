22eb46ffafc152d5fddd2b632735be3c0b0aa8bd
Syldris <131539895+Syldris@users.noreply.github.com>
2025-12-03 05:05:02 +0900

    [Silver V] Title: 지금 밥이 문제냐, Time: 56 ms, Memory: 6472 KB -BaekjoonHub

--- C#/백준/Silver/12787. 지금 밥이 문제냐/지금 밥이 문제냐.cs
+++ C#/백준/Silver/12787. 지금 밥이 문제냐/지금 밥이 문제냐.cs
@@ -1,4 +1,6 @@
 #nullable disable
+using System.Runtime.ConstrainedExecution;
+
 class Program
 {
     static void Main()
@@ -20,36 +22,20 @@ class Program
                 int[] arr = line[1].Split('.').Select(int.Parse).Reverse().ToArray();
                 ulong result = 0;
 
-                for (int v = 0; v < 8; v++)
+                for (int i = 0; i < 8; i++)
                 {
-                    for (int i = 0; i < 8; i++)
-                    {
-                        int bit = (arr[v] >> i & 1);
-                        if (bit == 1)
-                        {
-                            result |= 1ul << (v * 8 + i);
-                        }
-                    }
+                    result |= (ulong)arr[i] << (i * 8);
                 }
-
                 sw.WriteLine(result);
             }
             else
             {
                 ulong value = ulong.Parse(line[1]);
                 int[] result = new int[8];
-                for (int v = 0; v < 8; v++)
+                for (int i = 0; i < 8; i++)
                 {
-                    int cur = 0;
-                    for (int i = 0; i < 8; i++)
-                    {
-                        int bit = (int)(value >> (v * 8 + i) & 1);
-                        if (bit == 1)
-                        {
-                            cur |= 1 << i;
-                        }
-                    }
-                    result[7 - v] = cur;
+                    int bit = (int)(value >> (i * 8) & 0b11111111);
+                    result[7 - i] = bit;
                 }
                 sw.WriteLine(String.Join('.', result));
             }
