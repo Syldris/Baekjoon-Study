92e34bc2a7fe5f46c9853964174468e9a9bb7d93
Syldris <131539895+Syldris@users.noreply.github.com>
2025-12-03 12:36:04 +0900

    [Platinum V] Title: 부분배열 고르기, Time: 112 ms, Memory: 23524 KB -BaekjoonHub

--- C#/백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.cs
+++ C#/백준/Platinum/2104. 부분배열 고르기/부분배열 고르기.cs
@@ -9,17 +9,16 @@ class Program
         int n = int.Parse(sr.ReadLine());
         string[] input = sr.ReadLine().Split();
         int[] arr = new int[n + 1];
-
+        long[] prefix = new long[n + 1];
         for (int i = 1; i <= n; i++)
         {
             arr[i] = int.Parse(input[i - 1]);
+            prefix[i] = prefix[i-1] + arr[i];
         }
 
         int[] mintree = new int[n * 4];
-        long[] sumtree = new long[n * 4];
 
         MinBuild(1, 1, n);
-        SumBuild(1, 1, n);
 
         sw.Write(ScoreQuery(1, n));
 
@@ -36,18 +35,6 @@ class Program
             int rightNode = MinBuild(node * 2 + 1, mid + 1, end);
 
             return mintree[node] = arr[leftNode] < arr[rightNode] ? leftNode : rightNode;
-        }
-
-        long SumBuild(int node, int start, int end)
-        {
-            if (start == end)
-            {
-                return sumtree[node] = arr[start];
-            }
-
-            int mid = (start + end) / 2;
-
-            return sumtree[node] = SumBuild(node * 2, start, mid) + SumBuild(node * 2 + 1, mid + 1, end);
         }
 
         int MinQuery(int node, int start, int end, int left, int right)
@@ -81,22 +68,6 @@ class Program
             }
         }
 
-        long SumQuery(int node, int start, int end, int left, int right)
-        {
-            if (start > right || end < left)
-            {
-                return 0;
-            }
-
-            if (left <= start && end <= right)
-            {
-                return sumtree[node];
-            }
-
-            int mid = (start + end) / 2;
-            return SumQuery(node * 2, start, mid, left, right) + SumQuery(node * 2 + 1, mid + 1, end, left, right);
-        }
-
         long ScoreQuery(int start, int end)
         {
             if (start > end)
@@ -109,7 +80,8 @@ class Program
             }
 
             int minindex = MinQuery(1, 1, n, start, end);
-            long sum = SumQuery(1, 1, n, start, end);
+
+            long sum = prefix[end] - prefix[start - 1];
 
             long value = arr[minindex] * sum;
 
