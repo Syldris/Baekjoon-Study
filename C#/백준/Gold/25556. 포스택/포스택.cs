027c48a259e38c4a37fc2fa5d49a7a2be0547296
Syldris <131539895+Syldris@users.noreply.github.com>
2025-12-22 08:46:56 +0900

    [Gold V] Title: 포스택, Time: 68 ms, Memory: 13364 KB -BaekjoonHub

--- C#/백준/Gold/25556. 포스택/포스택.cs
+++ C#/백준/Gold/25556. 포스택/포스택.cs
@@ -9,21 +9,16 @@ class Program
         int n = int.Parse(sr.ReadLine());
         int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
 
-        Stack<int>[] stacks = new Stack<int>[4];
-        for (int i = 0; i < 4; i++)
-        {
-            stacks[i] = new Stack<int>();
-            stacks[i].Push(0);
-        }
+        int[] num = new int[4];
 
         for (int i = 0; i < n; i++)
         {
             bool push = false;
             for (int j = 0; j < 4; j++)
             {
-                if (stacks[j].Peek() <= arr[i])
+                if (num[j] <= arr[i])
                 {
-                    stacks[j].Push(arr[i]);
+                    num[j] = arr[i];
                     push = true;
                     break;
                 }
