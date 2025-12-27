23355854727519c8c0d5d5a7c34c69dec7dad70d
Syldris <131539895+Syldris@users.noreply.github.com>
2025-12-28 06:03:32 +0900

    [Silver II] Title: 나무 자르기, Time: 352 ms, Memory: 96332 KB -BaekjoonHub

---
+++ C#/백준/Silver/2805. 나무 자르기/나무 자르기.cs
@@ -0,0 +1,63 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        using StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);
+        int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+        int start = 0, end = 1000000000;
+        
+        while (start < end)
+        {
+            int mid = (start + end) / 2;
+            long value = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (mid >= arr[i])
+                {
+                    continue;
+                }
+                value += arr[i] - mid;
+                if (value > k)
+                {
+                    break;
+                }
+            }
+            
+            if (value == k)
+            {
+                sw.Write(mid);
+                return;
+            }
+            else if (value > k)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        long result = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (start >= arr[i])
+            {
+                continue;
+            }
+            result += arr[i] - start;
+        }
+
+        if (result < k)
+        {
+            start--;
+        }
+
+        sw.Write(start);
+    }
+}
