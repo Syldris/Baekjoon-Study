a83d027d025a5a800936c6ef22e43bf3cc118c52
Syldris <131539895+Syldris@users.noreply.github.com>
2025-12-31 06:20:14 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 300 ms, Memory: 24856 KB -BaekjoonHub

--- C#/백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
+++ C#/백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cs
@@ -3,13 +3,13 @@ class Program
 {
     static void Main()
     {
-        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 18);
-        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 18);
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1<<20);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1<<20);
 
         int n = int.Parse(sr.ReadLine());
         for (int i = 0; i < n; i++)
         {
-            int[] line = Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+            int[] line = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
             sw.WriteLine(line[0] + line[1]);
         }
     }
