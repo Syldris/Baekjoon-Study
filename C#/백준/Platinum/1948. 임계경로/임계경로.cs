f49ab7e5ccff213b0dfec5a2ae735a5d6dae0d34
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-02 01:15:12 +0900

    [Platinum V] Title: 임계경로, Time: 1440 ms, Memory: 23976 KB -BaekjoonHub

---
+++ C#/백준/Platinum/1948. 임계경로/임계경로.cs
@@ -0,0 +1,92 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        int n = int.Parse(sr.ReadLine());
+        int m = int.Parse(sr.ReadLine());
+
+        List<(int node, int cost)>[] graph = new List<(int node, int cost)>[n + 1];
+        List<int>[] prev = new List<int>[n + 1];
+
+        int[] dist = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            graph[i] = new List<(int node, int cost)>();
+            prev[i] = new List<int>();
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            int[] line = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+            int a = line[0];
+            int b = line[1];
+            int cost = line[2];
+            graph[a].Add((b, cost));
+        }
+
+        string[] input = sr.ReadLine().Split();
+        int start = int.Parse(input[0]);
+        int end = int.Parse(input[1]);
+        int value = 0;
+
+        PriorityQueue<(int node, int cost), int> queue = new();
+        queue.Enqueue((start, 0), 0);
+        while (queue.Count > 0)
+        {
+            (int node, int cost) = queue.Dequeue();
+
+            if (cost < dist[node])
+                continue;
+
+            if (node == end)
+            {
+                if (cost > value)
+                {
+                    value = cost;
+                }
+                continue;
+            }
+
+            foreach (var next in graph[node])
+            {
+                int nextcost = cost + next.cost;
+                if (nextcost > dist[next.node])
+                {
+                    prev[next.node].Clear();
+                    prev[next.node].Add(node);
+
+                    dist[next.node] = nextcost;
+                    queue.Enqueue((next.node, nextcost), -nextcost);
+                }
+                else if (nextcost == dist[next.node])
+                {
+                    prev[next.node].Add(node);
+                }
+            }
+        }
+
+        HashSet<(int from, int to)> hash = new HashSet<(int from, int to)>();
+        Queue<int> trackBack = new();
+
+        trackBack.Enqueue(end);
+        while (trackBack.Count > 0)
+        {
+            int node = trackBack.Dequeue();
+            foreach (var next in prev[node])
+            {
+                if (!hash.Contains((node, next)))
+                {
+                    hash.Add((node, next));
+                    trackBack.Enqueue(next);
+                }
+            }
+        }
+
+        sw.WriteLine(value);
+        sw.WriteLine(hash.Count);
+    }
+}
