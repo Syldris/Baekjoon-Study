20e1906a1e4615fc9142c5f093e04038e4423394
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-04 16:49:59 +0900

    [Gold I] Title: 버스 노선, Time: 2444 ms, Memory: 171596 KB -BaekjoonHub

--- C#/백준/Gold/12817. 버스 노선/버스 노선.cs
+++ C#/백준/Gold/12817. 버스 노선/버스 노선.cs
@@ -3,8 +3,8 @@ class Program
 {
     static void Main()
     {
-        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 17);
-        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 17);
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 20);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 18);
 
         int n = int.Parse(sr.ReadLine());
         List<int>[] tree = new List<int>[n + 1];
@@ -45,9 +45,8 @@ class Program
                 }
             }
 
-            bus[parent] += (n - size[node]) * size[node];
-
-            bus[node] += (n - size[node]) * size[node];
+            bus[node] += (n - size[node]) * size[node]; //위 => 아래 (n - size[node])개 지역에서 출발 => size[node]개 지역을 지나감
+            bus[parent] +=  size[node] * (n - size[node]); //아래 => 위 size[node]개 지역에서 출발 => (n - size[node])개 지역을 지나감
         }
     }
 }
