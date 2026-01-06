0f9c15bea9725470f46cace5cf7e333fe95b9b8a
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-06 11:36:06 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 1148 ms, Memory: 83496 KB -BaekjoonHub

---
+++ C#/백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cs
@@ -0,0 +1,120 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 18);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 18);
+        while (true)
+        {
+            string[] input = sr.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            if (n == 0 && m == 0)
+            {
+                return;
+            }
+
+            int[] parent = new int[n + 1];
+            long?[] dist = new long?[n + 1];
+
+            List<(int node, long value)>[] graph = new List<(int, long)>[n + 1];
+            List<(int a, int b, int index)> query = new List<(int, int, int)>();
+            long?[] reuslt = new long?[m];
+
+            for (int i = 1; i <= n; i++)
+            {
+                parent[i] = i;
+                graph[i] = new List<(int node, long value)>();
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] line = sr.ReadLine().Split();
+                if (line[0] == "!")
+                {
+                    int a = int.Parse(line[1]);
+                    int b = int.Parse(line[2]);
+                    int value = int.Parse(line[3]);
+
+                    Union(a, b, value);
+                    graph[a].Add((b, value));
+                    graph[b].Add((a, -value));
+                }
+                else
+                {
+                    int a = int.Parse(line[1]);
+                    int b = int.Parse(line[2]);
+
+                    if (Find(parent[a]) == Find(parent[b]))
+                    {
+                        query.Add((a, b, i));
+                    }
+                    else
+                    {
+                        reuslt[i] = long.MaxValue;
+                    }
+                }
+            }
+
+            foreach ((int a, int b, int index) in query)
+            {
+                if (!dist[a].HasValue || !dist[b].HasValue)
+                {
+                    DFS(a);
+                }
+                reuslt[index] = dist[b] - dist[a];
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                if (reuslt[i].HasValue)
+                {
+                    sw.WriteLine(reuslt[i] == long.MaxValue ? "UNKNOWN" : reuslt[i]);
+                }
+            }
+
+            void DFS(int start)
+            {
+                PriorityQueue<(int node, long value), long> queue = new();
+                dist[start] = 0;
+                queue.Enqueue((start, 0), 0);
+
+                while (queue.Count > 0)
+                {
+                    (int node, long value) = queue.Dequeue();
+                    foreach (var next in graph[node])
+                    {
+                        long nextValue = value + next.value;
+                        if (!dist[next.node].HasValue || nextValue < dist[next.node])
+                        {
+                            dist[next.node] = nextValue;
+                            queue.Enqueue((next.node, nextValue), nextValue);
+                        }
+                    }
+                }
+            }
+
+            int Find(int x)
+            {
+                if (parent[x] != x)
+                {
+                    parent[x] = Find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            void Union(int a, int b, int value)
+            {
+                int rootA = Find(a);
+                int rootB = Find(b);
+
+                if (rootA != rootB)
+                {
+                    parent[rootA] = rootB;
+                }
+            }
+        }
+    }
+}
