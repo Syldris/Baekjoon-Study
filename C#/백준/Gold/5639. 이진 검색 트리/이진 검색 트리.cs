43e97ecd7e3e5a632a2cfb6ea2ebaf2c56373422
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-10 06:38:13 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 336 ms, Memory: 7876 KB -BaekjoonHub

--- C#/백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cs
+++ C#/백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cs
@@ -1,11 +1,11 @@
 #nullable disable
 class Program
 {
-    class Node
+    struct Node
     {
         public int value;
-        public Node left;
-        public Node right;
+        public int left = 0;
+        public int right = 0;
 
         public Node(int value)
         {
@@ -19,43 +19,51 @@ class Program
         using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
 
         int first = int.Parse(sr.ReadLine());
+        Node[] tree = new Node[10001];
+        tree[0] = new Node(first);
+        int count = 1;
 
-        Node root = new Node(first);
         while (true)
         {
             string input = sr.ReadLine();
             if (input == null) break;
 
             int value = int.Parse(input);
-            Insert(root, value);
+            Insert(0, value);
         }
 
-        DFS(root);
+        DFS(tree[0]);
 
-        void Insert(Node node, int value)
+        void Insert(int index, int value)
         {
-            if (value < node.value)
+            if (value < tree[index].value)
             {
-                if (node.left != null) 
-                    Insert(node.left, value);
+                if (tree[index].left != 0)
+                    Insert(tree[index].left, value);
 
                 else
-                    node.left = new Node(value);
+                {
+                    tree[index].left = count;
+                    tree[count++] = new Node(value);
+                }
             }
             else
             {
-                if (node.right != null)
-                    Insert(node.right, value);
+                if (tree[index].right != 0)
+                    Insert(tree[index].right, value);
 
                 else
-                    node.right = new Node(value);
+                {
+                    tree[index].right = count;
+                    tree[count++] = new Node(value);
+                }
             }
         }
 
         void DFS(Node node)
         {
-            if (node.left != null) DFS(node.left);
-            if (node.right != null) DFS(node.right);
+            if (node.left != 0) DFS(tree[node.left]);
+            if (node.right != 0) DFS(tree[node.right]);
 
             sw.WriteLine(node.value);
         }
