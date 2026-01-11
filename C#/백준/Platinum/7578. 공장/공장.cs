0442fbd354122d40c8d66a4fde231eaf308530c3
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-11 17:32:49 +0900

    [Platinum V] Title: 공장, Time: 468 ms, Memory: 69376 KB -BaekjoonHub

---
+++ C#/백준/Platinum/7578. 공장/공장.cs
@@ -0,0 +1,79 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 20);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        int n = int.Parse(sr.ReadLine());
+
+        int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+        int[] arr2 = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+        const int range = 1000000;
+
+        int[] pos = new int[range + 1];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int item = arr2[i];
+            pos[item] = i + 1;
+        }
+
+        int[] tree = new int[range * 4];
+
+        long result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int item = arr[i];
+            int index = pos[item];
+
+            result += Query(index, range);
+            Update(index);
+        }
+
+        sw.Write(result);
+
+        void Update(int index, int node = 1, int start = 1, int end = range)
+        {
+            if (start == end)
+            {
+                tree[node]++;
+                return;
+            }
+
+            int mid = (start + end) / 2;
+
+            if (index <= mid)
+            {
+                Update(index, node << 1, start, mid);
+            }
+            else
+            {
+                Update(index, (node << 1) + 1, mid + 1, end);
+            }
+
+            tree[node] = tree[node << 1] + tree[(node << 1) + 1];
+        }
+
+        int Query(int left, int right, int node = 1, int start = 1, int end = range)
+        {
+            if (start > right || end < left)
+            {
+                return 0;
+            }
+
+            if (left <= start && end <= right)
+            {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+
+            return Query(left, right, node << 1, start, mid) + Query(left, right, (node << 1) + 1, mid + 1, end);
+        }
+
+    }
+}
