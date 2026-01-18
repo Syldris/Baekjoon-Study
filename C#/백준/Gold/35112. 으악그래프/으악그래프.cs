4b4e616fa5789aed23230e63f659bddeff447309
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-18 20:58:15 +0900

    [Gold V] Title: 으악그래프, Time: 52 ms, Memory: 5672 KB -BaekjoonHub

---
+++ C#/백준/Gold/35112. 으악그래프/으악그래프.cs
@@ -0,0 +1,22 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        string[] input = sr.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+
+        if (m > n)
+        {
+            sw.Write("No");
+        }
+        else
+        {
+            sw.Write("Yes");
+        }
+    }
+}
