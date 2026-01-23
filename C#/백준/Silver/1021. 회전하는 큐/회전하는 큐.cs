6a620c2b458f45e46bb81e5e419032ac9770a516
Syldris <131539895+Syldris@users.noreply.github.com>
2026-01-23 19:31:20 +0900

    [Silver III] Title: 회전하는 큐, Time: 60 ms, Memory: 6792 KB -BaekjoonHub

---
+++ C#/백준/Silver/1021. 회전하는 큐/회전하는 큐.cs
@@ -0,0 +1,56 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 18);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        string[] input = sr.ReadLine().Split();
+
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+
+        LinkedList<int> deque = new LinkedList<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            deque.AddLast(i);
+        }
+
+        int[] arr = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+        int result = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            int left = 0;
+            while (deque.First() != arr[i])
+            {
+                int node = deque.First();
+                deque.RemoveFirst();
+                deque.AddLast(node);
+                left++;
+            }
+
+            for (int k = 0; k < left; k++)
+            {
+                int node = deque.Last();
+                deque.RemoveLast();
+                deque.AddFirst(node);
+            }
+
+            int right = 0;
+            while (deque.First() != arr[i])
+            {
+                int node = deque.Last();
+                deque.RemoveLast();
+                deque.AddFirst(node);
+                right++;
+            }
+
+            deque.RemoveFirst();
+            result += Math.Min(left, right);
+        }
+
+        sw.Write(result);
+    }
+}
