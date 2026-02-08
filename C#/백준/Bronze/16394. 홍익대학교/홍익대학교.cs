189949e756ba882fef2f8e1588e56a90aa512f3e
Syldris <131539895+Syldris@users.noreply.github.com>
2026-02-09 06:00:03 +0900

    [Bronze V] Title: 홍익대학교, Time: 44 ms, Memory: 5544 KB -BaekjoonHub

---
+++ C#/백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,13 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        int n = int.Parse(sr.ReadLine());
+        sw.Write(n - 1946);
+
+    }
+}
