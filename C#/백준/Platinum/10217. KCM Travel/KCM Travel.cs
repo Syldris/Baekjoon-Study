863f398a956673abfe92ad616a392fb56c8efb8f
Syldris <131539895+Syldris@users.noreply.github.com>
2026-02-13 15:43:02 +0900

    [Platinum IV] Title: KCM Travel, Time: 732 ms, Memory: 14548 KB -BaekjoonHub

--- C#/백준/Platinum/10217. KCM Travel/KCM Travel.cs
+++ C#/백준/Platinum/10217. KCM Travel/KCM Travel.cs
@@ -17,7 +17,7 @@ class Program
 {
     static void Main()
     {
-        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 18);
         using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
 
         int testcase = int.Parse(sr.ReadLine());
@@ -41,58 +41,47 @@ class Program
                 graph[line[0]].Add(new Node(line[1], line[2], line[3])); // 출발지역 => (도착지점, 비용, 시간)
             }
 
-            for (int i = 1; i <= n; i++)
-            {
-                graph[i].Sort((a, b) => a.time.CompareTo(b.time)); // 효율적 탐색을 위해 시간순으로 정렬
-            }
-
-            int[,] visited = new int[n + 1, m + 1]; // [지역, 현재 비용] => 시간
+            int[,] dp = new int[n + 1, m + 1]; // [지역, 현재 비용] => 시간
             for (int i = 1; i <= n; i++)
             {
                 for (int j = 0; j <= m; j++)
                 {
-                    visited[i, j] = int.MaxValue;
+                    dp[i, j] = int.MaxValue;
                 }
             }
 
-            PriorityQueue<Node, int> queue = new();
-            queue.Enqueue(new Node(1, 0, 0), 0);
-            visited[1, 0] = 0;
+            dp[1, 0] = 0;
 
-            bool find = false;
-
-            while (queue.Count > 0)
+            for (int cost = 0; cost <= m; cost++)
             {
-                Node node = queue.Dequeue();
-
-                if (node.time > visited[node.pos, node.cost]) continue;
-
-                if (node.pos == n)
+                for (int node = 1; node <= n; node++)
                 {
-                    sw.WriteLine(node.time);
-                    find = true;
-                    break;
-                }
+                    if (dp[node, cost] == int.MaxValue) continue; // 미 도착지점에선 출발금지!
 
-                foreach (var next in graph[node.pos])
-                {
-                    int nextCost = node.cost + next.cost;
-                    int nextTime = node.time + next.time;
+                    int time = dp[node, cost];
 
-                    if (nextCost > m) // 총 비용이 돈을 넘으면 못감
-                        continue;
-
-                    if (nextTime < visited[next.pos, nextCost])
+                    foreach (var edge in graph[node])
                     {
-                        visited[next.pos, nextCost] = nextTime;
+                        int nextTime = time + edge.time;
+                        int nextCost = cost + edge.cost;
 
-                        queue.Enqueue(new Node(next.pos, nextCost, nextTime), nextTime);
+                        if (nextCost > m) continue; // 비용초과시 이동불가능!
+
+                        if (nextTime < dp[edge.pos, nextCost])
+                        {
+                            dp[edge.pos, nextCost] = nextTime;
+                        }
                     }
                 }
             }
 
-            if (!find)
-                sw.WriteLine("Poor KCM");
+            int minTime = int.MaxValue;
+            for (int cost = 0; cost <= m; cost++)
+            {
+                minTime = Math.Min(minTime, dp[n, cost]);
+            }
+
+            sw.WriteLine(minTime == int.MaxValue ? "Poor KCM" : minTime);
         }
     }
 }
