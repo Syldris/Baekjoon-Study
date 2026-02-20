d44cb44a2783d5e650a74c6c0acd225e28d9f7c1
Syldris <131539895+Syldris@users.noreply.github.com>
2026-02-20 21:51:05 +0900

    [Platinum V] Title: 간선 끊어가기, Time: 120 ms, Memory: 21920 KB -BaekjoonHub

--- C#/백준/Platinum/14285. 간선 끊어가기/간선 끊어가기.cs
+++ C#/백준/Platinum/14285. 간선 끊어가기/간선 끊어가기.cs
@@ -11,11 +11,12 @@ class Program
         int m = int.Parse(input[1]);
 
         List<(int node, int cost)>[] graph = new List<(int, int)>[n + 1];
-        int[] visited = new int[n + 1];
+        int[,] visited = new int[n + 1, 2];
         for (int i = 1; i <= n; i++)
         {
             graph[i] = new();
-            visited[i] = int.MaxValue;
+            visited[i, 0] = int.MaxValue;
+            visited[i, 1] = int.MaxValue;
         }
 
         int totalCost = 0;
@@ -32,33 +33,48 @@ class Program
         int startPos = int.Parse(input2[0]);
         int endPos = int.Parse(input2[1]);
 
-        PriorityQueue<(int node, int cost, int maxCost), int> queue = new();
-        queue.Enqueue((startPos, 0, int.MinValue), 0);
-        visited[startPos] = 0;
+        PriorityQueue<(int node, int cost, int maxCost, bool isRemove), int> queue = new();
+        queue.Enqueue((startPos, 0, 0, false), 0);
+        visited[startPos, 0] = 0;
 
-        int removeCost = int.MaxValue; 
-
+        int removeCost = 0;
+        // 테스트용
         while (queue.Count > 0)
         {
-            (int node, int cost, int maxCost) = queue.Dequeue();
-
+            (int node, int cost, int maxCost, bool isRemove) = queue.Dequeue();
             if (node == endPos)
             {
-                removeCost = Math.Min(cost - maxCost, removeCost);
-                continue;
+                removeCost = cost; 
+                break;
             }
 
             foreach (var next in graph[node])
             {
                 int nextCost = cost + next.cost;
-                int nextMaxCost = Math.Max(next.cost, maxCost);
 
-                int nextRemoveCost = nextCost - nextMaxCost; // 경로중에서 가중치가 가장 큰 간선 하나만 제거
-                
-                if (nextRemoveCost < visited[next.node]) // 이 노드까지 왔을때 이득이 최대만 남께끔
+                if (!isRemove) // 아직 제거 안했을때
                 {
-                    visited[next.node] = nextRemoveCost;
-                    queue.Enqueue((next.node, nextCost, nextMaxCost), nextRemoveCost);
+                    int nextMaxCost = Math.Max(next.cost, maxCost);
+                    int nextRemoveCost = nextCost - nextMaxCost; // 경로중에서 가중치가 가장 큰 간선 하나만 제거
+
+                    if (nextRemoveCost < visited[next.node, 1]) // 제거하면서 가는 경우
+                    {
+                        visited[next.node, 1] = nextRemoveCost;
+                        queue.Enqueue((next.node, nextRemoveCost, nextMaxCost, true), nextRemoveCost);
+                    }
+                    if (nextCost < visited[next.node, 0]) // 제거 안하고 그대로 두는 경우
+                    {
+                        visited[next.node, 0] = nextCost;
+                        queue.Enqueue((next.node, nextCost, nextMaxCost, false), nextCost);
+                    }
+                }
+                else // 이미 제거 했을때
+                {
+                    if (nextCost < visited[next.node, 1])
+                    {
+                        visited[next.node, 1] = nextCost;
+                        queue.Enqueue((next.node, nextCost, maxCost, isRemove), nextCost);
+                    }
                 }
             }
         }
