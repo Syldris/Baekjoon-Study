1e670732c4693187d7141004b15c9db597957ea9
Syldris <131539895+Syldris@users.noreply.github.com>
2026-02-26 06:00:33 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 72 ms, Memory: 16140 KB -BaekjoonHub

--- C#/백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cs
+++ C#/백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main()
     {
-        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 18);
         using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
 
         int n = int.Parse(sr.ReadLine());
