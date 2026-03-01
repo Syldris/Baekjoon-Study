1bf5c5e761ddf57ac44816b3069ff32c4cd3c02c
Syldris <131539895+Syldris@users.noreply.github.com>
2026-03-02 07:17:49 +0900

    [Platinum V] Title: 보석 줍기, Time: 404 ms, Memory: 19024 KB -BaekjoonHub

--- C#/백준/Platinum/2001. 보석 줍기/보석 줍기.cs
+++ C#/백준/Platinum/2001. 보석 줍기/보석 줍기.cs
@@ -1,4 +1,6 @@
 #nullable disable
+using System.Numerics;
+
 class Program
 {
     static void Main()
@@ -11,11 +13,11 @@ class Program
         int m = int.Parse(input[1]);
         int k = int.Parse(input[2]);
 
-        int[] jewel = new int[n + 1];
+        uint[] jewel = new uint[n + 1];
         for (int i = 0; i < k; i++)
         {
             int pos = int.Parse(sr.ReadLine());
-            jewel[pos] = 1 << i;
+            jewel[pos] = (uint)1 << i;
         }
 
         bool[,] visited = new bool[n + 1, 1 << k]; // k = 3이면 0~7까지 표현해야하니 크기8 필요 즉 1<<k 만큼
@@ -31,9 +33,9 @@ class Program
             graph[line[1]].Add((line[0], line[2]));
         }
 
-        Queue<(int node, int curJewel)> queue = new();
+        Queue<(int node, uint curJewel)> queue = new();
 
-        int startJewel = jewel[1];
+        uint startJewel = jewel[1];
         if (startJewel > 0) // 시작지점에 보석이 있는경우
         {
             visited[1, startJewel] = true;
@@ -45,19 +47,21 @@ class Program
 
         while (queue.Count > 0)
         {
-            (int node, int curJewel) = queue.Dequeue();
+            (int node, uint curJewel) = queue.Dequeue();
             foreach (var next in graph[node])
             {
-                int weight = 0;
+                /*int weight = 0;
                 for (int i = 0; i < k; i++)
                 {
                     bool bit = ((curJewel >> i) & 1) == 1;
                     if (bit) weight++;
-                }
+                }*/
+
+                int weight = BitOperations.PopCount(curJewel); // 켜진 비트기 몇개인지 핀뱔히는 메소드다.
 
                 if (weight <= next.weight) // 현재 들고있는 무게가 다리보다 무겁지 않아야 이동가능.
                 {
-                    int nextJewel = curJewel | jewel[next.node]; // 보석은 이동하고나서 줍는다.
+                    uint nextJewel = curJewel | jewel[next.node]; // 보석은 이동하고나서 줍는다.
 
                     if (!visited[next.node, nextJewel]) // 줍고 지나갈때
                     {
@@ -76,18 +80,10 @@ class Program
 
         int result = 0;
         int size = 1 << k;
-        for (int i = 0; i < size; i++)
+        for (uint i = 0; i < size; i++)
         {
             if (visited[1, i])
-            {
-                int value = 0;
-                for (int j = 0; j < k; j++)
-                {
-                    bool bit = ((i >> j) & 1) == 1;
-                    if (bit) value++;
-                }
-                result = Math.Max(result, value);
-            }
+                result = Math.Max(result, BitOperations.PopCount(i));
         }
 
         sw.Write(result);
