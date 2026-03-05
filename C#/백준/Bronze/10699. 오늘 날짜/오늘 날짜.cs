ffe94849dcb8e59bf9218f5b590506224ab0c636
Syldris <131539895+Syldris@users.noreply.github.com>
2026-03-06 05:53:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 48 ms, Memory: 5540 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+public class NewEmptyCSharpScript
+{
+    static void Main()
+    {
+        DateTime today = DateTime.Today;
+
+        Console.WriteLine(today.ToString("yyyy-MM-dd"));
+    }
+}
