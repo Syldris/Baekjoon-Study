d86199d5145ab97fa3cc1044e42c8b75a6fb6301
Syldris <131539895+Syldris@users.noreply.github.com>
2026-03-08 05:59:25 +0900

    [Bronze V] Title: 새로운 시작, Time: 44 ms, Memory: 5408 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15962. 새로운 시작/새로운 시작.cs
@@ -0,0 +1,11 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        sw.Write("파이팅!!");
+    }
+}
