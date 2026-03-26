8caaf09131f8506b5bf4c423923af00638433cbf
Syldris <131539895+Syldris@users.noreply.github.com>
2026-03-27 03:50:34 +0900

    [Platinum IV] Title: 순열, Time: 92 ms, Memory: 15348 KB -BaekjoonHub

--- C#/백준/Platinum/1849. 순열/순열.cs
+++ C#/백준/Platinum/1849. 순열/순열.cs
@@ -11,7 +11,6 @@ class Program
         int[] result = new int[n];
         int[] tree = new int[n * 4];
 
-        Build(1, 1, n);
 
         for (int i = 1; i <= n; i++)
         {
@@ -24,35 +23,23 @@ class Program
             sw.WriteLine(result[i]);
         }
 
-        void Build(int node, int start, int end)
+
+        void KthUpdate(int node, int start, int end, int order, int value) // order = 0이 되게 순서에 맞는 빈공간에 Update 
         {
             if (start == end)
             {
-                tree[node] = 1; // 1로 채워서 몇개 차있는지 기록
-                return;
-            }
-
-            int mid = (start + end) >> 1;
-
-            Build(node << 1, start, mid);
-            Build((node << 1) + 1, mid + 1, end);
-            tree[node] = tree[node << 1] + tree[(node << 1) + 1];
-        }
-
-        void KthUpdate(int node, int start, int end, int order, int value) // order 순서에 맞는 빈공간에 Update
-        {
-            if (start == end)
-            {
-                tree[node] = 0; // 0으로 바꿔서 빈공간이 아님을 기록.
+                tree[node] = 1; // 1으로 바꿔서 빈공간이 아님을 기록.
                 result[start - 1] = value;
                 return;
             }
 
             int mid = (start + end) >> 1;
 
-            if (order >= tree[node << 1]) //남은 빈공간의 값보다 남은 순서가 크면 왼쪽노드 공간 다쓰고도 오른쪽으로 가야함
+            int leftEmptySize = mid - start + 1 - tree[node << 1]; // 왼쪽 노드 범위에서 기록한 1갯수만큼 뺌
+
+            if (order >= leftEmptySize) //남은 빈공간의 값보다 남은 순서가 크면 왼쪽노드 공간 다쓰고도 오른쪽으로 가야함
             {
-                KthUpdate((node << 1) + 1, mid + 1, end, order - tree[node << 1], value); // 남은순서 - 왼쪽빈공간으로 빈공간 2개있으면 4번째에서 2번째로 낮추기.
+                KthUpdate((node << 1) + 1, mid + 1, end, order - leftEmptySize, value); // 남은순서 - 왼쪽빈공간으로 빈공간 2개있으면 4번째에서 2번째로 낮추기.
             }
             else
             {
