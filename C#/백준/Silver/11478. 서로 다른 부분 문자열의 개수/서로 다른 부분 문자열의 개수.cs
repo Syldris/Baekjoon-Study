ce6fd3cf77b807becbc71253cc967d4110cdd153
Syldris <131539895+Syldris@users.noreply.github.com>
2026-04-05 06:15:20 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 860 ms, Memory: 396088 KB -BaekjoonHub

---
+++ C#/백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cs
@@ -0,0 +1,26 @@
+#nullable disable
+class Program
+{
+    static void Main()
+    {
+        using StreamReader sr = new StreamReader(Console.OpenStandardInput(), bufferSize: 1 << 16);
+        using StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1 << 16);
+
+        string text = sr.ReadLine();
+        HashSet<string> hash = new();
+
+        ReadOnlySpan<char> span = text.AsSpan();
+
+        for (int len = 1; len <= text.Length; len++)
+        {
+            for (int start = 0; start + len <= text.Length; start++)
+            {
+                string subtext = text.Substring(start, len);
+
+                hash.Add(subtext);
+            }
+        }
+        sw.Write(hash.Count);
+
+    }
+}
