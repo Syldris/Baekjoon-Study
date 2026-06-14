b369b389817fb31a75e6e04508586f5d16f3f09e
Syldris <131539895+Syldris@users.noreply.github.com>
2026-06-14 23:49:56 +0900

    [level 1] Title: 카드 뭉치, Time: 0.20 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution
+{
+    public string solution(string[] cards1, string[] cards2, string[] goal)
+    {
+        string answer = "";
+
+        int index1 = 0, index2 = 0;
+
+        for (int i = 0; i < goal.Length; i++)
+        {
+            if (index1 < cards1.Length && cards1[index1] == goal[i]) // 1번 카드 뭉치 읽기
+                index1++;
+            else if (index2 < cards2.Length && cards2[index2] == goal[i]) // 2번 카드 뭉치 읽기
+                index2++;
+            else
+                return "No";
+        }
+
+        return "Yes";
+    }
+}
