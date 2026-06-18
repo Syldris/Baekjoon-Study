d12f08e931283cc0340e74c16fb92a74569b8ff6
Syldris <131539895+Syldris@users.noreply.github.com>
2026-06-19 00:08:35 +0900

    [level 1] Title: 덧칠하기, Time: 0.54 ms, Memory: 47.2 MB -BaekjoonHub

--- C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
+++ C#/프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -1,29 +1,18 @@
 using System;
-
 public class Solution
 {
     public int solution(int n, int m, int[] section)
     {
         int answer = 0;
 
-        bool[] paint = new bool[n + 1];
-        Array.Fill(paint, true);
-        for (int i = 0; i < section.Length; i++)
-        {
-            paint[section[i]] = false;
-        }
-
         int num = -1;
 
-        for (int i = 1; i <= n; i++)
+        for (int i = 0; i < section.Length; i++)
         {
-            if (i <= num) continue; // 이미 칠해졌음
+            if (section[i] <= num) continue; // 이미 칠해졌음
 
-            if (!paint[i])
-            {
-                num = i + m - 1; // i칸부터 m칸만큼 칠했으니 m-1칸까진 전부 칠해짐
-                answer++;
-            }
+            num = section[i] + m - 1; // i칸부터 m칸만큼 칠했으니 m-1칸까진 전부 칠해짐
+            answer++;
         }
 
         return answer;
