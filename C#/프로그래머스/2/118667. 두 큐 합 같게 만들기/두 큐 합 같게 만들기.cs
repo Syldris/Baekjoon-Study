955adeb19c5fedcc71a4519bbe21860b6c432dfa
Syldris <131539895+Syldris@users.noreply.github.com>
2026-06-22 16:17:03 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.46 ms, Memory: 65.3 MB -BaekjoonHub

--- C#/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
+++ C#/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cs
@@ -21,7 +21,18 @@ public class Solution
             Queue2.Enqueue(queue2[i]);
         }
 
-        while (value1 != value2 && answer < n * 8) // 실제 Max 교환횟수는 어느정도 일까? 
+        /* Max 교환횟수는 어느정도 일까? 
+         * 두 큐의 삽입 삭제를 생각해보면 원형배열로도 나타낼수있다. 
+         * 큐 1의 범위를 | 큐1 원소들 | 로 나타내겟다. 큐2는 나머지 부분. 
+         * | 1 2 3 4 | 5 6 7 8  n=4 일때 시작상태다.
+         * 시작 구간을 l. 끝구간을 r 로 잡으면 l = 0, r = n 으로 시작한다.
+         * 두 큐 합이 같을떄 한쪽 큐가 빈 큐일수 없다는 성질을 생각하자.
+         * | 1 2 3 4 5 6 7 | 8 로 r은 총 n-1번 이동가능하다.
+         * 1 2 3 4 5 6 | 7 | 8 로 l은 위 상태(2n-1개 원소)에서 최대 2n-2번 움직임 가능하다.
+         * 고로 총 움직임은 3n-3번이다. l, r 모두 => 로 움직이는 원형배열의 회전으로 생각하면 된다.
+            */
+
+        while (value1 != value2 && answer < n * 3 - 3)
         {
             answer++;
             if (value1 > value2)
@@ -40,6 +51,6 @@ public class Solution
             }
         }
 
-        return answer < n * 8 ? answer : -1;
+        return value1 == value2 ? answer : -1;
     }
 }
